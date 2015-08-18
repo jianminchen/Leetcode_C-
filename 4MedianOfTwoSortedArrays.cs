129a545a463914e305df0cb33a91dd391af9fee6
Jianmin Chen <jianminchen.fl@gmail.com>
2015-08-17 22:11:04 -0700

    C# implementation

--- 4MedianOfTwoSortedArrays.cs
+++ 4MedianOfTwoSortedArrays.cs
@@ -43,33 +43,71 @@ namespace _4MedianOfTwoSortedArrays
          * 需要进行k次。但是如果每次我们都剔除一半呢？所以用这种类似于二分的思想，我们可以这样
          * 考虑：
          * 
+         * Julia's comment: 
+         * 1. Quickly review what we should do in the function
+         * step 1: input arguments, two array, 2 length, keep small length array in the first one; 
+         *         switch the call if need. - reduce if case discussion
+         * step 2: base cases - 
+         *         case 1: m==0, kth element is b[k-1]
+         *         case 2: k==1, kth element is to compare first element in two sorted array
+         * step 3: divide k into two parts, memorize first part of k: minimum (k / 2, m)
+         *    2 tips to remember: 
+         *       1. first, it should not be bigger than short array length m, 
+         *          should be less than k /2 as well. 
+         * step 4: need to remove first part of array a or first part of array b,
+         *     recall how to make the decision: 
+         *     compare, and then, decide: 
+         * step 5: recursive call in 3 cases of if statement 
+         * 
+         * 
          */
-        double findKth(int[] a, int m, int[] b, int n, int k)  
-        {  
+        public static double findKth(int[] a, int m, int[] b, int n, int k)
+        {
             //always assume that m is equal or smaller than n  
-            if (m > n)  
-                return findKth(b, n, a, m, k);  
+            if (m > n)                              // step 1
+                return findKth(b, n, a, m, k);
 
-            if (m == 0)  
-                return b[k - 1];  
+            if (m == 0)                 // step 2
+                return b[k - 1];
 
-            if (k == 1)  
-                return Math.Min(a[0], b[0]);  
+            if (k == 1)                 // step 2 
+                return Math.Min(a[0], b[0]);
 
             //divide k into two parts  
             int pa = Math.Min(k / 2, m), pb = k - pa;
 
             if (a[pa - 1] < b[pb - 1])
             {
-
-                return findKth(a + pa, m - pa, b, n, k - pa);
+                int[] aShift = arrayShift(a, pa);
+                return findKth(aShift, m - pa, b, n, k - pa);
             }
             else if (a[pa - 1] > b[pb - 1])
             {
-                return findKth(a, m, b + pb, n - pb, k - pb);
+                int[] bShift = arrayShift(b, pb);
+                return findKth(a, m, bShift, n - pb, k - pb);
             }
             else
-                return a[pa - 1];  
-        }  
+                return a[pa - 1];
+        }
+
+        /*
+         * a short version of arrayShift in C#
+         * 
+         */
+        public static int[] arrayShift(int[] a, int n)
+        {
+            int len = a.Length;
+
+            if (n <= len)
+            {
+                int[] outA = new int[len - n];
+                for (int i = 0; i < len - n; i++)
+                    outA[i] = a[n + i];
+
+                return outA; 
+            }
+
+            return null; 
+        }
     }
 }
